5404bd85f9543e3382592fbec75263aa6f560863
konstantin <konstantin.grbic@gmail.com>
2022-11-28 13:42:31 -0500

    add testing on success for init

---
+++ test.py
@@ -0,0 +1,23 @@
+import git, unittest, os, shutil
+
+class TestInitSuccess(unittest.TestCase):
+
+	def setUp(self):
+		git.init('temp')
+		
+	def test_success(self):
+		self.assertTrue(os.path.isdir('temp'))
+		self.assertTrue(os.path.isdir('temp/.git'))
+		self.assertTrue(os.path.isdir('temp/.git/objects'))
+		self.assertTrue(os.path.isdir('temp/.git/refs'))
+		self.assertTrue(os.path.isdir('temp/.git/refs/heads'))
+		self.assertTrue((os.path.exists('temp/.git/HEAD'))
+		
+	def tearDown(self):
+		shutil.rmtree('temp')
+	
+class TestMethodsError(unittest.TestCase):
+	pass
+	
+if __name__ == '__main__':
+	unittest.main()
