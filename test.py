4cb2777fc923fb4e5176b8575f2caaba9b055eae
konstantin <konstantin.grbic@gmail.com>
2022-11-29 10:34:03 -0500

    typo

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class TestInitSuccess(unittest.TestCase):
 		self.assertTrue(os.path.isdir('temp/.git/objects'))
 		self.assertTrue(os.path.isdir('temp/.git/refs'))
 		self.assertTrue(os.path.isdir('temp/.git/refs/heads'))
-		self.assertTrue((os.path.exists('temp/.git/HEAD'))
+		self.assertTrue(os.path.exists('temp/.git/HEAD'))
 		
 	def tearDown(self):
 		shutil.rmtree('temp')
