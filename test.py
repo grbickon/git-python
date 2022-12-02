815ca5f1e29fd957bdf38f821c80ea88ea41d134
konstantin <konstantin.grbic@gmail.com>
2022-12-02 12:45:13 -0500

    refactor hash testing + add new tests

--- test.py
+++ test.py
@@ -29,15 +29,25 @@ class TestHashObjectSuccess(unittest.TestCase):
 
 	def setUp(self):
 		git.init('temp')
-		
-	def testEmptyBlob(self):
-		# no data, type 'blob'
-		sha1 = 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391'
-		self.assertEqual(git.hash_object(b'','blob',True), sha1)
-		path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
-		self.assertTrue(os.path.exists(path))
-		self.assertEqual(git.read_file(path), zlib.compress(b'blob 0\x00'))
+			# hash with no data of type 'blob', type 'commit', type 'tree' respectively	
+		self.types = ['blob', 'commit', 'tree']
+		self.hashes = ['e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
+					   'dcf5b16e76cce7425d0beaef62d79a7d10fce1f5',
+					   '4b825dc642cb6eb9a060e54bf8d69288fbee4904']
 
+	def testHashing(self):
+		for i in range(len(self.types)):
+			self.assertEqual(git.hash_object(b'',self.types[i],False), self.hashes[i])
+
+
+	def testWritingToFiles(self):
+		for i in range(len(self.types)):
+			git.hash_object(b'',self.types[i],True)
+			path = os.path.join('.git', 'objects', self.hashes[i][:2], self.hashes[i][2:])
+			self.assertTrue(os.path.exists(path))
+			self.assertEqual(git.read_file(path), zlib.compress((self.types[i] + ' 0\x00').encode()))
+		
+		
 	def tearDown(self):
 		shutil.rmtree('temp')
 	
